b543559462d79296d8990f96fac45922f6f4dd99
GLKaiky <kaikyfranca8@gmail.com>
2023-10-06 14:32:32 -0300

    Removido servidor, adicionado chat

--- codigo/chat.js
+++ codigo/chat.js
@@ -1,23 +1,114 @@
-const ws = new WebSocket('ws://localhost:8080/servidor')
-const input = document.querySelector('input');
-const output = document.querySelector('output');
+const apiKey = "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"
 
-ws.addEventListener('open', console.log)
-ws.addEventListener('message', console.log)
+//Definir frases aleatorias
 
-ws.addEventListener('message', message =>{
-    const dados = JSON.parse(message.data);
-    if(dados.type === 'chat'){
-        output.append('Outro: ' + dados.text, document.createElement('br'));
-    }
-})
+const Frases = [
+    "batata!",
+    "fasddj",
+    "kaskdkasdk",
+    "lasldasldsl"
 
-input.addEventListener('keypress', e => {
-    if (e.code === 'Enter') {
-        const valor = input.value;
-        output.append( 'Eu:' + valor, document.createElement('br'));
-        ws.send(valor);
+]
 
-        input.value = '';
-    }
-})
+//Fim definir
+
+function gerarFrase(){
+    const indice = Math.floor(Math.random() * Frases.length);
+    return Frases[indice];
+}
+
+
+
+//fim
+
+function sendMessage(){
+    var message = document.getElementById('message-input');
+    if(!message.value){
+        message.style.border = '1px solid red'
+        return
+    }    
+    message.style.border = "none"
+
+    //requisição da mensagem digitada pelo usuário
+    
+    var userMessage = message.value;
+    showHistoric(userMessage)
+    message.value = ' ';
+
+    //fim
+
+    var status = document.getElementById('status');
+    var button = document.getElementById('button');
+    button.disabled = true;
+    button.style.cursor = 'not-allowed'
+
+    setTimeout(function(){
+        button.disabled = false;
+        button.style.cursor = 'pointer';
+    }, 2000); 
+}
+
+function showHistoric(message){
+    var historic = document.getElementById('historic')
+
+    //minhas mensagens
+
+    var boxMyMessage = document.createElement('div')
+    boxMyMessage.className = 'box-my-message'
+
+    var MyMessage = document.createElement('p')
+    MyMessage.className = 'my-message'
+    MyMessage.innerHTML = message
+
+    boxMyMessage.appendChild(MyMessage)
+    historic.appendChild(boxMyMessage)
+
+    //respostas 
+
+    var RespostaMessage = document.createElement('div')
+    RespostaMessage.className = 'box-response-message'
+
+    var chatResponse = document.createElement('p')
+    chatResponse.className = 'chat-response'
+
+
+   
+
+
+    setTimeout(function(){
+        historic.removeChild(AnimationMessage)
+        chatResponse.innerHTML = gerarFrase();
+        chatResponse.style.marginLeft = '-5%'
+    },3000);
+
+    RespostaMessage.appendChild(chatResponse)
+    historic.appendChild(RespostaMessage)
+
+
+     //animação
+
+     var AnimationMessage = document.createElement('div')
+     AnimationMessage.className = 'box-animation-message'
+    
+     var animation = document.createElement('p')
+     animation.className = 'animação';
+ 
+ 
+     AnimationMessage.appendChild(animation)
+     historic.appendChild(AnimationMessage)
+ 
+ 
+     //fim animação
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
