41c984274efed7d61fc2c6032bc44f2b81c74277
KaiohZz <kaio.khls.pkm@Gmail.com>
2023-11-14 11:42:00 -0300

    Implementação da página inicial

---
+++ codigo/script.js
@@ -0,0 +1,9 @@
+document.getElementById("login").addEventListener("click",redirectLogin());
+document.getElementById("cadastro").addEventListener("click",redirectCadastro());
+document.getElementById("forum").addEventListener("click",redirectForum());
+
+function redirectLogin() {window.location = "/login/login.html"}
+
+function redirectCadastro() {window.location = "/login/login.htmls"}
+
+function redirectForum() {window.location = "/homepage/main.html"}
